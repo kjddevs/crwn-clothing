ed0d7f67b0ce080f6029e88f3c02cd2f74c2c266
kjd2502 <kjd2502@hotmail.com>
2020-05-20 18:08:09 +0100

    Add basic menu items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Homepage from "./homepage.components";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">Empty</div>
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
   );
 }
 
